5611ce80a590ac9888057888540261646a591994
Onno Steenbergen <onno@steenbe.nl>
2016-01-30 14:01:05 +0100

    Fixed #15 Check for player mode

--- plugins/queue.py
+++ plugins/queue.py
@@ -49,9 +49,13 @@ def queue_next(message, cmd, *args):
         return
     username = slackify.client.users[message.body["user"]]['name']
     current_queue = slackify.queue.get_queue()
-    if slackify.player.current == None or current_queue == None or current_queue.user == username or slackify.player.mode == QueueManager.MODE_RANDOM:
-        slackify.queue.current(nxt.id)
-        slackify.player.play(nxt.song, slackify.queue.MODE_QUEUE)
+    if slackify.player.current == None or \
+        current_queue == None or \
+        current_queue.user == username or \
+        slackify.player.mode == slackify.player.MODE or \
+        slackify.player.mode == QueueManager.MODE_RANDOM:
+            slackify.queue.current(nxt.id)
+            slackify.player.play(nxt.song, slackify.queue.MODE_QUEUE)
     elif current_queue != None and current_queue.user != username:
         message.reply('Can only skip songs you queued')
 
