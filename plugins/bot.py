bc4807f8a39137a6b8d70c969825da36d6bda658
Onno Steenbergen <onno@steenbe.nl>
2016-08-13 17:53:14 +0200

    Added album art (fixes #14), and some more help
    Signed-off-by: Onno Steenbergen <onno@steenbe.nl>

--- plugins/bot.py
+++ plugins/bot.py
@@ -3,6 +3,14 @@ from lib.singleton import Slackify
 
 slackify = Slackify()
 
+@respond_to('^(show|hide) album art$')
+def album_art(message,show, *args):
+    slackify.bot.show_album_art = show == "show"
+    if slackify.bot.show_album_art:
+        message.reply("Showing album art")
+    else:
+        message.reply("Hiding album art")
+
 @respond_to('^reconnect$')
 def reconnect(message,*args):
     slackify.client.reconnect()
@@ -16,6 +24,10 @@ def listen(message, *args):
 @respond_to('help')
 def help(message, *args):
     message.reply("""Available commands:
+- `start`: start playing
+- `stop`: stop playing
+- `pause`: pause playing
+- `play`: continue playing
 - `play QUERY`: play a song now
 - `next`: Skip current song
 - `queue QUERY`: queue a song
@@ -24,4 +36,6 @@ def help(message, *args):
 - `play NUMBER`: play song from search result
 - `queue NUMBER`: queue song from search result
 - `remove NUMBER`: Remove number from queue/playlist
+- `show album art`: Display album art when starting a song
+- `hide album art`: Do not show album art
 """)
